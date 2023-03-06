857477125f020c2ce29544cc931d470b531cb347
seth <rader.seth@gmail.com>
2023-03-05 23:02:06 -0500

    WIP: Invert x-direction

--- Quake/r_world.c
+++ Quake/r_world.c
@@ -208,7 +208,7 @@ static void R_InitBModelInstance (bmodel_gpu_instance_t *inst, entity_t *ent)
 	MatrixTranspose4x3 (mat, inst->world);
 
 	inst->alpha = ent->alpha == ENTALPHA_DEFAULT ? -1.f : ENTALPHA_DECODE (ent->alpha);
-	inst->scroll[0] = ent->model->scrollv[0] * (float)realtime;
+	inst->scroll[0] = -ent->model->scrollv[0] * (float)realtime;
 	inst->scroll[1] = ent->model->scrollv[1] * (float)realtime;
 	memset (&inst->padding, 0, sizeof(inst->padding));
 }
